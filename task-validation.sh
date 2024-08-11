48874fe20c97434d6ab07b14efcc56d3e4d3040a
Badr-1 <badrmohamedashraf@gmail.com>
2024-08-11 03:49:22 +0300

    ✨ Colorfull prints

--- task-validation.sh
+++ task-validation.sh
@@ -1,5 +1,46 @@
 #!/bin/env bash
 
+print_message() {
+    # Define colors with bold and semantic meaning
+    BOLD_RED='\033[1;31m'     # Error
+    BOLD_GREEN='\033[1;32m'   # Success
+    BOLD_YELLOW='\033[1;33m'  # Warning
+    BOLD_BLUE='\033[1;34m'    # Information
+    BOLD_CYAN='\033[1;36m'    # Highlight
+    BOLD_MAGENTA='\033[1;35m' # Important
+    BOLD_WHITE='\033[1;37m'   # Neutral
+    RESET='\033[0m'
+    local type=$1
+    local message=$2
+
+    case $type in
+    error)
+        echo -e "${BOLD_RED}Error: ${message}${RESET}"
+        ;;
+    success)
+        echo -e "${BOLD_GREEN}Success: ${message}${RESET}"
+        ;;
+    warning)
+        echo -e "${BOLD_YELLOW}Warning: ${message}${RESET}"
+        ;;
+    info)
+        echo -e "${BOLD_BLUE}Info: ${message}${RESET}"
+        ;;
+    highlight)
+        echo -e "${BOLD_CYAN}Highlight: ${message}${RESET}"
+        ;;
+    important)
+        echo -e "${BOLD_MAGENTA}Important: ${message}${RESET}"
+        ;;
+    neutral)
+        echo -e "${BOLD_WHITE}Neutral: ${message}${RESET}"
+        ;;
+    *)
+        echo -e "${BOLD_WHITE}Neutral: ${message}${RESET}"
+        ;;
+    esac
+}
+
 get_repo() {
     github_link=$(echo "$entry" | cut -d "," -f2)
     reponame="$(echo $github_link | sed 's/\.git$//' | awk -F '/' '{print $NF}')"
